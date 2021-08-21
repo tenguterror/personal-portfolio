5e03d3445cc043d5a2e176fa480a585d8598b83d
TenguTerror <vegasduard@gmail.com>
2021-08-20 22:07:22 -0600

    Removed border change from main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ let switchDark = (dark, borderDark) => {
   dark = document.body;
   borderDark = document.getElementById('aboutme-container');
   dark.classList.toggle('dark-mode');
-  borderDark.style.border = '1px solid white';
 }
 
 switchMode.onclick = switchDark;

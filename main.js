5b1b1f41a767ec52738ae068d470cfd45af2573c
TenguTerror <vegasduard@gmail.com>
2021-08-24 16:40:18 -0600

    edited the elemnt from js that is not needed

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let switchMode = document.getElementById('button');
 
 let switchDark = (dark, borderDark) => {
   dark = document.body;
-  borderDark = document.getElementById('aboutme-container');
   dark.classList.toggle('dark-mode');
 }
 

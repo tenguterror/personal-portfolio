660dbeed00727133ff6dbf68686ca8da5398cb05
TenguTerror <vegasduard@gmail.com>
2021-08-29 16:11:41 -0600

    refactoring code in js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let switchMode = document.getElementById("button");
 
 const switchDark = (switchMode.onclick = function () {
-  const old = document.getElementById("button").innerHTML;
+  let mode = document.getElementById("button").innerHTML;
   let body = document.body;
-  if (old === 'Dark Mode') {
-    document.getElementById("button").innerHTML = 'Light Mode';
+  if (mode === 'Dark Mode') {
+    mode = 'Light Mode';
     body.classList.remove('dark-mode');
-  } else if (old === 'Light Mode') {
-    document.getElementById("button").innerHTML = 'Dark Mode';
+  } else if (mode === 'Light Mode') {
+    mode = 'Dark Mode';
     body.classList.toggle("dark-mode");
   } else {
     console.log('ERROR');

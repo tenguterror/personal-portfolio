3207abd9730ac54349307c9f62338fa043b863e1
TenguTerror <vegasduard@gmail.com>
2021-08-29 15:12:58 -0600

    Made js switch text with light and dark mode

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 let switchMode = document.getElementById("button");
 
-let switchDark = (dark) => {
-  dark = document.body;
-  dark.classList.toggle("dark-mode");
-};
-
-switchMode.onclick = switchDark;
+const switchDark = (switchMode.onclick = function () {
+  const old = document.getElementById("button").innerHTML;
+  let body = document.body;
+  if (old === 'Dark Mode') {
+    document.getElementById("button").innerHTML = 'Light Mode';
+    body.classList.remove('dark-mode');
+  } else if (old === 'Light Mode') {
+    document.getElementById("button").innerHTML = 'Dark Mode';
+    body.classList.toggle("dark-mode");
+  } else {
+    console.log('ERROR');
+  }
+});

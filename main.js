f6b130690ddbfe948184d57d57a2bf85f2c100a4
Duard <vegasduard@gmail.com>
2022-03-15 15:11:46 -0600

    formatting through prettier

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const switchMode = document.getElementById("button");
+const switchMode = document.getElementById('button');
 
 const switchDark = (switchMode.onclick = () => {
-  let mode = document.getElementById("button").innerHTML;
+  let mode = document.getElementById('button').innerHTML;
   let body = document.body;
   if (mode === 'Dark Mode') {
-    document.getElementById("button").innerHTML = 'Light Mode';
+    document.getElementById('button').innerHTML = 'Light Mode';
     body.classList.remove('dark-mode');
   } else if (mode === 'Light Mode') {
-    document.getElementById("button").innerHTML = 'Dark Mode';
-    body.classList.toggle("dark-mode");
+    document.getElementById('button').innerHTML = 'Dark Mode';
+    body.classList.toggle('dark-mode');
   } else {
     throw Error('Something went wrong!');
   }
